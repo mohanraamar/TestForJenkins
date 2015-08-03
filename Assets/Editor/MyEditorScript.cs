b52d5549da4faa704ee74c52b861081b9651b591
MohanRaam <mohanraam@system-admins-Mac-mini.local>
2015-08-03 15:28:15 +0530

    For jenkins test for unity build

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+class MyEditorScript {
+	static string[] SCENES = FindEnabledEditorScenes();
+	
+	static string APP_NAME = "YourProject1";
+	static string TARGET_DIR = "target";
+	
+	[MenuItem ("Custom/CI/Build Mac OS X")]
+	static void PerformMacOSXBuild ()
+	{
+		string target_dir = APP_NAME + ".app";
+		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSXUniversal,BuildOptions.None);
+	}
+
+	[MenuItem ("Custom/CI/Build Android")]
+	static void PerformAndroidBuild ()
+	{
+		string target_dir = APP_NAME + "";
+		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android,BuildOptions.None);
+	}
+	
+	private static string[] FindEnabledEditorScenes() {
+		List<string> EditorScenes = new List<string>();
+		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+			if (!scene.enabled) continue;
+			EditorScenes.Add(scene.path);
+		}
+		return EditorScenes.ToArray();
+	}
+	
+	static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+		string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
+		if (res.Length > 0) {
+			throw new Exception("BuildPlayer failure: " + res);
+		}
+	}
+}
