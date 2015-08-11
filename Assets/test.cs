3273a7de3387289e9e8b291de6148a34fca80700
MohanRaam <mohanraam@system-admins-Mac-mini.local>
2015-08-11 11:17:40 +0530

    11 Aug 2015

---
+++ Assets/test.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	void OnGUI () {
+		GUI.Label (new Rect(Screen.width/2,0,100,200), "Jenkins Test Success");
+	}
+}
